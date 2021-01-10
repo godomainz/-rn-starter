5b1d1f6207128e918f7707aee17bc784b91bd76e
Akila Mendis <akila@oneit.com.au>
2021-01-10 13:45:35 +0530

    011 Showing a Custom Component

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import HomeScreen from "./src/screens/HomeSceen";
+import HomeScreen from './src/screens/HomeSceen';
+import ComponentsScreen from './src/screens/ComponentsScreen';
 
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen
+    Home: HomeScreen,
+    Components: ComponentsScreen
   },
   {
-    initialRouteName: "Home",
+    initialRouteName: "Components",
     defaultNavigationOptions: {
       title: "App"
     }
