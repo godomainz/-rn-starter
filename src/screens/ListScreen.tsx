f4a277350b97ac1cdc22839e1cfe19db9ff53503
Akila Mendis <akila@oneit.com.au>
2021-01-10 14:11:22 +0530

    017 Building Lists

---
+++ src/screens/ListScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const ListScreen = () => {
+
+    return (
+        <Text>Hello from ListScreen</Text>
+    );
+
+}
+const styles = StyleSheet.create({});
+
+export default ListScreen;
