acdc6141a37d4962055b3933c4ab3ed5066faaa1
Akila Mendis <akila@oneit.com.au>
2021-01-10 14:33:04 +0530

    019 Rendering a FlatList

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
-
+    const friends = [
+        {name: 'Friend 1'},
+        {name: 'Friend 2'},
+        {name: 'Friend 3'},
+        {name: 'Friend 4'},
+        {name: 'Friend 5'},
+        {name: 'Friend 6'},
+        {name: 'Friend 7'},
+        {name: 'Friend 8'},
+        {name: 'Friend 9'}
+    ];
     return (
-        <Text>Hello from ListScreen</Text>
+        <FlatList 
+            data={friends} 
+            renderItem={({item})=>{
+                return <Text>{item.name}</Text>
+            }}/>
     );
 
 }
