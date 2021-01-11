bc9f957bc37b8b839303d6a5eecdde647e444ebc
Akila Mendis <akila@oneit.com.au>
2021-01-11 16:34:29 +0530

    073 Flex Values

--- src/screens/BoxScreen.tsx
+++ src/screens/BoxScreen.tsx
@@ -5,9 +5,9 @@ const BoxScreen = (props:any) => {
 
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>Child #1</Text>
-            <Text style={styles.textStyle}>Child #2</Text>
-            <Text style={styles.textStyle}>Child #3</Text>
+            <Text style={styles.textOneStyle}>Child #1</Text>
+            <Text style={styles.textTwoStyle}>Child #2</Text>
+            <Text style={styles.textThreeStyle}>Child #3</Text>
         </View>
     );
 
@@ -17,13 +17,22 @@ const styles = StyleSheet.create({
     viewStyle:{
         borderWidth: 3,
         borderColor: 'black',
-        justifyContent: 'space-around',
-        flexDirection: 'row',
         height: 200
     },
-    textStyle:{
+    textOneStyle:{
         borderWidth: 3,
         borderColor: 'red',
+        flex: 4
+    },
+    textTwoStyle:{
+        borderWidth: 3,
+        borderColor: 'red',
+        flex: 4
+    },
+    textThreeStyle:{
+        borderWidth: 3,
+        borderColor: 'red',
+        flex: 2
     }
 });
 
