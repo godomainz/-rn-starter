1a3ebd03ab34695a4c17df80a110e10b22e36153
Akila Mendis <akila@oneit.com.au>
2021-01-11 15:48:04 +0530

    068 Layout with React Native

---
+++ src/screens/BoxScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const BoxScreen = (props:any) => {
+
+    return (
+        <Text>Hello from BoxScreen</Text>
+    );
+
+}
+
+const styles = StyleSheet.create({});
+
+export default BoxScreen;
--- src/screens/HomeSceen.tsx
+++ src/screens/HomeSceen.tsx
@@ -26,6 +26,9 @@ const HomeScreen = ({ navigation }:any) => {
       <Button
         onPress={() => navigation.navigate("Text")} 
         title="Go to Text demo"/>
+      <Button
+        onPress={() => navigation.navigate("Box")} 
+        title="Go to Box demo"/>
     </View>
   );
 };
