93c005af7d6c29913afb42b253a843b28dd72dcf
Akila Mendis <akila@oneit.com.au>
2021-01-11 15:04:41 +0530

    062 Showing a Text Input

--- src/screens/TextScreen.tsx
+++ src/screens/TextScreen.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, TextInput } from 'react-native';
 
 const TextScreen = (props:any) => {
 
     return (
-        <Text>Hello from TextScreen</Text>
+        <View>
+            <TextInput style={styles.input} />
+        </View>
     );
 
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1
+    }
+});
 
 export default TextScreen;
