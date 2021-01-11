761308ca9ec7323b0ca61d914f03449fd5f79e3a
Akila Mendis <akila@oneit.com.au>
2021-01-11 15:33:09 +0530

    067 Exercise Solution

--- src/screens/TextScreen.tsx
+++ src/screens/TextScreen.tsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import { Text, StyleSheet, View, TextInput } from 'react-native';
 
 const TextScreen = (props:any) => {
-    const [ name, setName ] = useState<string>('');
-
+    const [ password, sePassword ] = useState<string>('');
+    
     return (
         <View>
-            <Text>Enter Name:</Text>
-            <TextInput style={styles.input} autoCapitalize="none" autoCorrect={false} value={name} onChangeText={(newValue)=>setName(newValue)}/>
-            <Text>My Name is {name}</Text>
+            <Text>Enter Password:</Text>
+            <TextInput style={styles.input} autoCapitalize="none" autoCorrect={false} value={password} onChangeText={(newValue)=>sePassword(newValue)}/>
+            { password.length <= 5 ? <Text>Password must be longer than 5 characters</Text> : null }
         </View>
     );
 
