b6a0afa2890adfde5683262ebdafb70f2f7ee3f3
Akila Mendis <akila@oneit.com.au>
2021-01-11 09:29:26 +0530

    047 App Overview

---
+++ src/screens/SquareScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const SquareScreen = () => {
+
+    return (
+        <Text>Hello from SquareScreen</Text>
+    );
+
+}
+
+const styles = StyleSheet.create({});
+
+export default SquareScreen;
